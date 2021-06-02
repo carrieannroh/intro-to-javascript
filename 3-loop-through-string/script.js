0a488cdb709af7c0a725d6cf104a3e2bb8b09c53
Carrie <carriearoberts95@gmail.com>
2021-06-02 16:41:28 +0100

    Committing my solution to 3

--- 3-loop-through-string/script.js
+++ 3-loop-through-string/script.js
@@ -4,8 +4,9 @@
 
 let word = "LoLlapAlOoZa";
 
-for (letter of word) {
+for (let letter of word) {
   // set capitalised_letter to uppercase of letter
   let capitalisedLetter = letter.toUpperCase(); // common to use camelCase variable names in JS
   // print capitalised_letter
-  console.log(capitalisedLetter);
+  console.log(capitalisedLetter); 
+}
