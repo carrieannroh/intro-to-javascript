da166d50d2c75b7818206699ba1d4c6c00c6a132
Carrie <carriearoberts95@gmail.com>
2021-06-02 16:42:20 +0100

    committing my solution to 1

--- 1-expressions-with-variables/script.js
+++ 1-expressions-with-variables/script.js
@@ -2,10 +2,10 @@
 
 // TODO: fix me
 
-message = "This is an exercise in JavaScript variables and expressions! :)";
+let message =("This is an exercise in JavaScript variables and expressions!");
 console.log(message);
 
 let num = 10;
-console.log("{num} squared is...");
-let num = num * num;
-console.log(num);
+console.log(`{num} squared is...`);
+let num2 = num * num;
+console.log(num2);
